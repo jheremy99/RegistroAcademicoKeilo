fda61b86d5ee4c1b2a2828fcd93556fe0037cccc
Jheremy <77696486+jheremy99@users.noreply.github.com>
2025-10-28 23:30:39 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: "/RegistroAcademicoKeilo/", // 👈 ESTA LÍNEA ES LA IMPORTANTE
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
